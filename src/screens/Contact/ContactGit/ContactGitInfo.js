dbb71e262f68220cb9ed02ecb46b9869ac0c96e3
David Hansson <hansson.d.david@gmail.com>
2020-05-14 00:21:38 +0200

    See latest commits on 4hansson

--- src/screens/Contact/ContactGit/ContactGitInfo.js
+++ src/screens/Contact/ContactGit/ContactGitInfo.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+
+import trans from "constants/lang";
+import { LangContext } from "components/LangContext/LangContext";
 
 export default function ContactGitInfo() {
-    return (
-        <div style={{ width: "50%" }}></div>
-    );
+    const [state, setState] = useState(false);
+    const [data, setData] = useState();
+    const [lang] = useContext(LangContext);
+    const link = "https://github.com/DavidDHansson/4hansson/commits/master"
+
+    useEffect(() => {
+        fetch("https://api.github.com/repos/daviddhansson/4hansson/commits/master")
+            .then(res => res.json())
+            .then(json => { setData(json); setState(true) })
+            .catch(() => setState(false));
+    }, []);
+
+    function displayLatest(msg, name, info, url) {
+        return (
+            <div>
+                <p style={{ fontSize: 30, textAlign: "justify" }}>
+                    {lang === trans.lang[0] && (trans.contantGitTitle[0])}
+                    {lang === trans.lang[1] && (trans.contantGitTitle[1])}
+                </p>
+                <div className="contactGitProfileCommitBox">
+                    <div className="contactGitProfileCommitBoxLeft">
+                        <span
+                            style={{ fontWeight: 600, color: "rgb(55, 58, 65)", paddingBottom: "5px", fontSize: "18px" }}
+                        >{msg}</span>
+                        <span style={{ fontWeight: 600 }}>{name}
+                            <span style={{ fontWeight: "normal", color: "rgb(90, 96, 105)" }}> {formatData(info)}</span>
+                        </span>
+                    </div>
+                    <div className="contactGitProfileCommitBoxRight" onClick={() => window.open(url)}>C</div>
+                </div>
+            </div>
+        );
+    }
+
+    function formatData(date) {
+        return date.split("T")[0]
+    }
+
+    if (data != null && state) {
+        return (
+            <div style={{ width: "50%" }}>
+                {displayLatest(data.commit.message, data.commit.author.name, data.commit.author.date, link)}
+            </div>
+        )
+    } else {
+        return (
+            <div style={{ width: "50%" }}>
+                {displayLatest("API Error", "Error", "Too many Github API Request", link)}
+            </div>
+        );
+    }
 }
--- src/screens/Contact/ContactGit/ContactGitProfile.js
+++ src/screens/Contact/ContactGit/ContactGitProfile.js
@@ -24,30 +24,31 @@ export default class ContactGitProfile extends React.Component {
         const { error, isLoaded, data } = this.state;
 
         return error || data.message
-            ? <div>Woops, an error occurred</div>
+            ? <div style={{ width: "50%"}}><p>Woops, an error occurred. <br /> There were probably too many requests to the Github API</p></div>
             : !isLoaded
                 ? <div>Loading...</div>
-                : <Reveal onReveal={() => log({ site: "Contact", section: "2"})}>
+                : <Reveal onReveal={() => log({ site: "Contact", section: "2" })}>
                     <div className="contactGitProfileWrapper">
+                        <div>
+                            <div><img src={data.avatar_url} className="contactGitProfileImg" alt="Github Avatar" /></div>
 
-                        <div><img src={data.avatar_url} className="contactGitProfileImg" alt="Github Avatar" /></div>
+                            <div style={{ height: "1ch" }}><p style={{ fontWeight: 700, fontSize: "30px" }}>{data.name}</p></div>
+                            <div style={{
+                                height: "1ch",
+                                paddingBottom: "2ch",
+                                borderBottom: "1px solid rgb(150, 150, 150)",
+                                width: "50%"
+                            }}><p style={{ color: "rgb(90, 90, 90)", fontSize: "26px" }}>{data.login}</p></div>
 
-                        <div style={{ height: "1ch" }}><p style={{ fontWeight: 700, fontSize: "30px" }}>{data.name}</p></div>
-                        <div style={{
-                            height: "1ch",
-                            paddingBottom: "2ch",
-                            borderBottom: "1px solid rgb(150, 150, 150)",
-                            width: "50%"
-                        }}><p style={{ color: "rgb(90, 90, 90)", fontSize: "26px" }}>{data.login}</p></div>
+                            <div style={{ height: "1ch", paddingBottom: "1ch" }}><p>{data.bio}</p></div>
 
-                        <div style={{ height: "1ch", paddingBottom: "1ch" }}><p>{data.bio}</p></div>
-
-                        <div style={{ height: "1ch" }}><p style={{ fontWeight: 700 }}>{data.company}</p></div>
-                        <div style={{ height: "1ch", paddingBottom: "3ch" }}><p>{data.location}</p></div>
+                            <div style={{ height: "1ch" }}><p style={{ fontWeight: 700 }}>{data.company}</p></div>
+                            <div style={{ height: "1ch", paddingBottom: "3ch" }}><p>{data.location}</p></div>
 
 
-                        <div style={{ height: "1ch" }} className="contactGitProfileLink">
-                            <a target="_blank" rel="noopener noreferrer" href={`https://github.com/${data.login}`}>https://github.com/{data.login}</a>
+                            <div style={{ height: "1ch" }} className="contactGitProfileLink">
+                                <a target="_blank" rel="noopener noreferrer" href={`https://github.com/${data.login}`}>https://github.com/{data.login}</a>
+                            </div>
                         </div>
                     </div>
                 </Reveal>
