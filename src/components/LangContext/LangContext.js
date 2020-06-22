95f4268ffeece1494168eeb8501e01a851f49e35
David Hansson <hansson.d.david@gmail.com>
2020-06-22 23:57:24 +0200

    Language option is now saved in localstorage

--- src/components/LangContext/LangContext.js
+++ src/components/LangContext/LangContext.js
@@ -1,12 +1,25 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 export const LangContext = React.createContext({
     lang: "",
-    changeLang: () => {}
+    changeLang: () => { }
 });
 
 export const LangProvider = props => {
     const [lang, setLang] = useState("dk");
+
+    useEffect(() => {
+        if (localStorage.getItem("lang") === null) {
+            const saved = localStorage.getItem("lang");
+            if (saved === "dk" || saved === "en") {
+                setLang(saved);
+                console.log("updated")
+            }
+        } else {
+            localStorage.setItem("lang", "dk");
+            console.log("set")
+        }
+    }, []);
 
     return (
         <LangContext.Provider value={[lang, setLang]}>
--- src/components/langChange/LangChange.js
+++ src/components/langChange/LangChange.js
@@ -9,10 +9,15 @@ import gb from "./gb.svg";
 export default function LangChange() {
     const setLang = useContext(LangContext)[1];
 
+    function didTap(lang) {
+        localStorage.setItem("lang", lang);
+        setLang(lang);
+    }
+
     return (
         <div className="flagParent">
-            <div className="flagChild" onClick={() => setLang("en") }><img src={gb} className="flagChildImage" alt="United Kingdom"/></div>
-            <div className="flagChild" onClick={() => setLang("dk") }><img src={dk} className="flagChildImage" alt="Denmark"/></div>
+            <div className="flagChild" onClick={() => didTap("en") }><img src={gb} className="flagChildImage" alt="United Kingdom"/></div>
+            <div className="flagChild" onClick={() => didTap("dk") }><img src={dk} className="flagChildImage" alt="Denmark"/></div>
         </div>
     );
 }
